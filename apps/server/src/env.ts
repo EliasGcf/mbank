377d90bb01f82810a0cc2d3918dca09b2c178b8e
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-08-11 16:05:38 -0300

    chore: save graphql schema to file

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const env = z
   .object({
+    NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
     DATABASE_URL: z.string().url(),
     PORT: z.coerce.number().default(3333),
     JWT_SECRET: z.string(),
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,7 +1,15 @@
 import { app } from '@app';
 import { env } from '@env';
+import { printSchema } from 'graphql';
+import fs from 'node:fs/promises';
+
+import { schema } from '@graphql/schema';
 
 app.listen(3333, () => {
   console.log(`Server is running on port http://localhost:${env.PORT}`);
   console.log(`GraphQL playground is running on http://localhost:${env.PORT}/playground`);
+
+  if (env.NODE_ENV === 'development') {
+    fs.writeFile('src/graphql/schema.graphql', printSchema(schema));
+  }
 });
